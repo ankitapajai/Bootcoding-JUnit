b8e75c8343f3b35fa3e36c6613c6ba36a84b9477
ankitapajai <ankitapajai@gmail.com>
2023-09-04 09:34:41 +0530

    In Junit First ArraySum program

---
+++ src/main/java/com/bootcoding/junit/Array/ArraySum.java
@@ -0,0 +1,29 @@
+package com.bootcoding.junit.Array;
+
+public class ArraySum {
+    public int getSum(int numbers[]){
+        int sum = 0;
+        for(int n : numbers){
+            sum = sum + n;
+        }
+        return sum;
+    }
+
+    //print maximum elements...
+    public int getMaxElement(int numbers[]){
+        int sum = 0;
+        for(int n1 : numbers){
+            sum = sum + n1;
+        }
+        return sum;
+    }
+
+    //print even elements in an array...
+    public int evenElement(int numbers[]){
+        int sum = 0;
+        for(int n2 : numbers){
+            sum = sum + n2;
+        }
+        return sum;
+    }
+}
